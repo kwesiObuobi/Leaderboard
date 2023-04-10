698c0e746eabd9fe4f83ca59f6cdbe6ad32b0cd3
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-04-10 17:07:47 +0000

    Add scores.js file

---
+++ src/scores.js
@@ -0,0 +1,9 @@
+export default [
+  { name: 'user 1', score: 100 },
+  { name: 'user 2', score: 20 },
+  { name: 'user 2', score: 50 },
+  { name: 'user 2', score: 78 },
+  { name: 'user 2', score: 125 },
+  { name: 'user 2', score: 77 },
+  { name: 'user 2', score: 42 },
+];
