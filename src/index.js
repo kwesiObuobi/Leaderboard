4eb5a1794fd8a4dbc61d36e92277e852054283be
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-04-11 12:56:23 +0000

    Add code for addScore.js

--- src/index.js
+++ src/index.js
@@ -28,3 +28,11 @@ import showScores from './modules/showScores.js';
 
 const refresh = document.querySelector('.refresh');
 refresh.addEventListener('click', () => showScores);
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#name').value;
+  const score = document.querySelector('#input-score').value;
+  console.log(name);
+});
---
+++ src/modules/addScore.js
@@ -0,0 +1,18 @@
+import { BaseUrl, GameID } from '../config/api.js';
+
+export default async (user, score) => {
+  const response = await fetch(`${BaseUrl}games/${GameID}/scores`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      name: user,
+      score: Number(score),
+    }),
+  });
+
+  const result = await response.json();
+
+  return result;
+};
